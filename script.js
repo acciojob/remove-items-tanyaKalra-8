828e6163143ca688ac194ebe139c1d74be0bb91f
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-22 17:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function deleteElement() {
+	let element =  document.getElementByTagName("select")[0];	
+	let selectedElement = element.value;
+	
+	let childrenList = element.children;
+
+	for (let i = 0; i < childrenList.length; i++) {
+		if (selectedElement === childrenList[i].value) {
+			childrenList[i].remove();
+		}
+	}
+}
